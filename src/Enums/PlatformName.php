d36b18a116a4470bacfce6f80ef6a3169b5ae79a
Manpreet Singh <itmanpreet94@gmail.com>
2026-07-15 08:50:04 +0530

    update the code

---
+++ src/Enums/PlatformName.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DcyphrDigital\Helpers\Enums;
+
+enum PlatformName: string
+{
+    case Klaviyo = 'klaviyo';
+    case TripleWhale = 'triple_whale';
+    case Sendgrid = 'sendgrid';
+    case Crm = 'crm';
+    case Website = 'website';
+    case WebsiteUI = 'website_ui';
+    case Stock = 'stock';
+    case Bazaar = 'bazaar';
+    case CampaignMonitor = 'campaign_monitor';
+    case DataSftp = 'data_sftp';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
