7c1e1522f2b6f958ca091d01b3205c689015085a
Manpreet Singh <itmanpreet94@gmail.com>
2026-07-15 08:40:22 +0530

    update the code

---
+++ src/Support/CleansUpDirectory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DcyphrDigital\Helpers\Support;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use function App\Helpers\now;
+
+trait CleansUpDirectory
+{
+    private function cleanDirectory(string $storagePath, int $olderThanDays = 3, ?string $nameContains = null): void
+    {
+        collect(
+            Storage::disk('local')->files($storagePath)
+        )->each(function ($file) use ($olderThanDays, $nameContains) {
+            if ($nameContains && ! Str::contains($file, $nameContains)) {
+                return;
+            }
+            if (now()->subDays($olderThanDays)->startOfDay()->timestamp > Storage::disk('local')->lastModified($file)) {
+                Storage::disk('local')->delete($file);
+            }
+        });
+    }
+}
